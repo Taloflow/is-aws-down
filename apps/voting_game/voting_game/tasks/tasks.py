0fbd15aabdbf577c178703d80d28428158d8e795
Anu Joy <anujoy@gmail.com>
2022-01-10 23:03:39 +0000

    Update voting game tasks to read session token from env variables

--- apps/voting_game/voting_game/tasks/tasks.py
+++ apps/voting_game/voting_game/tasks/tasks.py
@@ -21,6 +21,7 @@ broker = SQSBroker(
     namespace="dramatiq_sqs_voting_game",
     aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
+    aws_session_token=os.environ['AWS_SESSION_TOKEN'],
     region_name=os.environ['AWS_REGION'],
     endpoint_url=os.environ['AWS_SQS_ENDPOINT_URL'],
     middleware=[
@@ -37,6 +38,7 @@ dramatiq.set_broker(broker)
 db = dynamodb.DynamoDBModel(
     aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
+    aws_session_token=os.environ['AWS_SESSION_TOKEN'],
     region_name=os.environ['AWS_REGION'],
     endpoint_url=os.environ['DYNAMODB_ENDPOINT_URL']
 )

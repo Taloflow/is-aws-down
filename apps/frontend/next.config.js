c467f5356675d509b6542d4b8f15358f4cf33194
Austin Buckler <austin@taloflow.ai>
2022-06-01 11:59:56 -0600

    added root url redirect to /is-aws-down

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,6 +1,18 @@
 const isProd = process.env.NODE_ENV === "production";
 
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = {
   // Use the CDN in production and localhost for development.
   assetPrefix: isProd ? "https://is-aws-down.pages.dev" : "",
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/is-aws-down',
+        permanent: false
+      }
+    ]
+  }
 };
--- apps/frontend/src/pages/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <>Home page will not be accessible</>;
-}

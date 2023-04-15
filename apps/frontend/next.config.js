323376e8f68898b4a2c519744027d595840c94b2
Austin Buckler <austin@taloflow.ai>
2023-04-14 20:43:45 -0600

    added explicit asset prefix

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,4 +1,4 @@
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
 
 /**
  * @type {import('next').NextConfig}
@@ -7,4 +7,5 @@ module.exports = {
   // Use the CDN in production and localhost for development.
   basePath: '/is-aws-down',
   // assetPrefix: isProd ? "https://is-aws-down.pages.dev" : ""
+  assetPrefix: '/is-aws-down/'
 };

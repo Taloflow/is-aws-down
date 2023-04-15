474a26774647db597880d611dac9b3d9fef74b9a
Austin Buckler <austin@taloflow.ai>
2023-04-14 19:21:01 -0600

    commented out more redux

--- apps/frontend/src/app/hooks.ts
+++ apps/frontend/src/app/hooks.ts
@@ -1,12 +1,13 @@
-import {
-    TypedUseSelectorHook,
-    useDispatch,
-    useSelector,
-} from 'react-redux';
-import type {
-    AppDispatch,
-    RootState,
-} from './store';
+// import {
+//     TypedUseSelectorHook,
+//     useDispatch,
+//     useSelector,
+// } from 'react-redux';
+// import type {
+//     AppDispatch,
+//     RootState,
+// } from './store';
 
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+// export const useAppDispatch = () => useDispatch<AppDispatch>();
+// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export {}
--- apps/frontend/src/app/store.ts
+++ apps/frontend/src/app/store.ts
@@ -1,39 +1,41 @@
-import {
-  Action,
-  combineReducers,
-  configureStore,
-  ThunkAction,
-} from "@reduxjs/toolkit";
-import votingGameReducer from "../features/votingGame/votingGameSlice";
-import metricsReducer from "../features/metricGraph/metricGraphSlice";
+// import {
+//   Action,
+//   combineReducers,
+//   configureStore,
+//   ThunkAction,
+// } from "@reduxjs/toolkit";
+// import votingGameReducer from "../features/votingGame/votingGameSlice";
+// import metricsReducer from "../features/metricGraph/metricGraphSlice";
 
-import { votingGameAPI } from "../features/votingGame/votingGameAPI";
-import { metricsAPI } from "../features/metricGraph/metricsGraphAPI";
-import { summaryAPI } from "../features/summaryPage/summaryAPI";
+// import { votingGameAPI } from "../features/votingGame/votingGameAPI";
+// import { metricsAPI } from "../features/metricGraph/metricsGraphAPI";
+// import { summaryAPI } from "../features/summaryPage/summaryAPI";
 
-const rootReducer = combineReducers({
-  metricsReducer: metricsReducer,
-  votingGameReducer: votingGameReducer,
-  [votingGameAPI.reducerPath]: votingGameAPI.reducer,
-  [metricsAPI.reducerPath]: metricsAPI.reducer,
-  [summaryAPI.reducerPath]: summaryAPI.reducer,
-});
+// const rootReducer = combineReducers({
+//   metricsReducer: metricsReducer,
+//   votingGameReducer: votingGameReducer,
+//   [votingGameAPI.reducerPath]: votingGameAPI.reducer,
+//   [metricsAPI.reducerPath]: metricsAPI.reducer,
+//   [summaryAPI.reducerPath]: summaryAPI.reducer,
+// });
 
-export const store = configureStore({
-  reducer: rootReducer,
-  devTools: true,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware()
-      .concat(votingGameAPI.middleware)
-      .concat(metricsAPI.middleware)
-      .concat(summaryAPI.middleware),
-});
+// export const store = configureStore({
+//   reducer: rootReducer,
+//   devTools: true,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware()
+//       .concat(votingGameAPI.middleware)
+//       .concat(metricsAPI.middleware)
+//       .concat(summaryAPI.middleware),
+// });
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+// export type AppDispatch = typeof store.dispatch;
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppThunk<ReturnType = void> = ThunkAction<
+//   ReturnType,
+//   RootState,
+//   unknown,
+//   Action<string>
+// >;
+
+export {}

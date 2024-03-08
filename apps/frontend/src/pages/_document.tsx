1176cd14a4f39ff5609c2cf98c643af499760dfd
caio <caioaoque@gmail.com>
2024-03-08 19:58:11 -0300

    Fix?

--- apps/frontend/src/pages/_document.tsx
+++ apps/frontend/src/pages/_document.tsx
@@ -5,11 +5,11 @@ import { Router } from "next/router";
 const DEFAULT_WRITE_KEY = "pXZJ3AzXJmrxGPRiWNqSpEDQdobjqWkf";
 
 function renderSnippet() {
-  return (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-NSCHQZH');
+  return `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-NSCHQZH')`;
 }
 
 Router.events.on("routeChangeComplete", (url) => {

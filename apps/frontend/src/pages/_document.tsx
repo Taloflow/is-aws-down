7383d12b013b7270988fe1d553365510ee35c296
caio <caioaoque@gmail.com>
2024-03-08 19:51:46 -0300

    Google Tag Manager

--- apps/frontend/src/pages/_document.tsx
+++ apps/frontend/src/pages/_document.tsx
@@ -1,7 +1,16 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import * as snippet from "@segment/snippet";
 import { Router } from "next/router";
 
 const DEFAULT_WRITE_KEY = "pXZJ3AzXJmrxGPRiWNqSpEDQdobjqWkf";
+
+function renderSnippet() {
+  return (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-NSCHQZH');
+}
 
 Router.events.on("routeChangeComplete", (url) => {
   // @ts-ignore
@@ -15,13 +24,10 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        // Google Tag Manager
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-NSCHQZH');</script>
-        // End Google Tag Manager
+          <script
+            id={"google-tag-manager-script"}
+            dangerouslySetInnerHTML={{ __html: renderSnippet() }}
+          />
         </Head>
         <body>
           <Main />
